93190d5b10e3976a62b208aa3bad4e517180bef3
sree9020554372 <98376271+sree9020554372@users.noreply.github.com>
2022-04-25 08:30:02 +0545

    Add files via upload

---
+++ Calc (2).java
@@ -0,0 +1,14 @@
+public class Calc{
+    public static void main(String args[]){
+	    int a=15;
+        int b=5;
+        int add=a+b;
+		int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+		System.out.println("sum = " + add);
+		System.out.println("sub = " + sub);
+		System.out.println("mul = " + mul);
+		System.out.println("division = " + div);
+}
+}
